0c18e5a00493946d4cfba8393639ec402907f928
fcody <fcody@fcodys-MacBook.local>
2021-07-14 16:25:56 +0300

    works

--- main.c
+++ main.c
@@ -6,37 +6,34 @@ int main(int ac, char **av, char **envp)
     int in;
     int out;
     pid_t pid;
-
+    //int stdout = dup(1);
     if(ac != 5)
     {
         printf("pipex: usage: ./pipex file1 cmd1 cmd2 file2\n");
         exit (1);
     }
+    char **a = ft_split(av[2], ' ');
+    char **b = ft_split(av[3], ' ');
+
     pipe(pfd);
     pid = fork();
 
-    char **a = ft_split(av[2], ' ');
-    char **b = ft_split(av[3], ' ');
     if(pid == 0)
     {
-        close(pfd[0]);
-        dup2(pfd[1], 1);
-        close(pfd[1]);
         in = open(av[1],O_RDONLY);
         dup2(in, 0);
-        execve(a[0], a, envp);
+        dup2(pfd[1],1);
+        execve(a[0],a, envp);
     }
-    else
-    {
-        out = open(av[4], O_RDWR, O_CREAT,O_TRUNC);
+//    else
+//    {
         wait(0);
-        dup2(pfd[0], 0);
         close(pfd[1]);
-        close(pfd[0]);
-
+//        waitpid(pid, 0, 0);
+        out = open(av[4], O_RDWR, O_CREAT, O_TRUNC);
         dup2(out, 1);
-        if (execve(b[0], b, envp) == -1)
-            printf("Error");
-    }
-    return 0;
+        dup2(pfd[0], 0);
+        execve(b[0], b, envp);
+//    }
+//    return 0;
 }
